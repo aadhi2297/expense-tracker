2af2ead6cdcd196aec5f3be5491c20b560371ddb
Adi Shankar Gubbala <adigubbala@gmail.com>
2025-09-17 14:34:51 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "https://expense-tracker-frontend-h5nu.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
